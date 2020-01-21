6a2fb0f7efa646ba2bb56819bb40c0ad44edd0c1
Corey Hammond <coreyjhammond@gmail.com>
2020-01-20 16:42:05 -0800

    getRandomLowerCase() created

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Javascript Password Generator
+
+```
+String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+```
---
+++ main.js
@@ -0,0 +1,7 @@
+// Generator functions
+
+function getRandomLowerCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+console.log(getRandomLowerCase());
