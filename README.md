bffde103327752f26792d66b8ca82e20c3ab16ec
Corey Hammond <coreyjhammond@gmail.com>
2020-01-20 18:23:47 -0800

    completed project

--- README.md
+++ README.md
@@ -1,5 +1,55 @@
 # Javascript Password Generator
 
+- Use String.fromCharCode to grab random values from browser character chart
+
 ```
-String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+function getRandomLowerCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpperCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+```
+
+- Simple way to parse a string to int
+
+```
+const length = +lengthEl.value;
+```
+
+- Good examples of working with booleans
+
+```
+const typesCount = lower + upper + number + symbol; // stores a value of 1 for each true value, and 0 for false
+const typesArr = [{ lower }, { upper }, { number }, { symbol }]; // creates an array of objects with the name as the key and the boolean value as its value
+```
+
+- Copy text to the clipboard
+
+```
+clipboardEl.addEventListener("click", () => {
+  const textarea = document.createElement("textarea");
+  const password = result.innerText;
+
+  if (!password) {
+    return;
+  }
+
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  alert("Password copied to clipboard");
+});
 ```
--- main.js
+++ main.js
@@ -1,7 +1,102 @@
-// Generator functions
+// DOM Elements
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
+
+// Object to store random functions
+const randomFunc = {
+  lower: getRandomLowerCase,
+  upper: getRandomUpperCase,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+// Events
+
+generateEl.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+// Copy password to clipboard
+clipboardEl.addEventListener("click", () => {
+  const textarea = document.createElement("textarea");
+  const password = result.innerText;
+
+  if (!password) {
+    return;
+  }
+
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  alert("Password copied to clipboard");
+});
+
+// Generate password functio
+function generatePassword(lower, upper, number, symbol, length) {
+  // 1. Init password var
+  let generatedPassword = "";
+
+  // 2. Filter out unchecked types
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    // I learned here that the [0] in the below statement refers to the first value of the object "item". If "item" had 2 key/values, then [1] would refer to the second value, and so on"
+    item => Object.values(item)[0]
+  );
+  // check if typesCount is empty
+  if (typesCount === 0) {
+    return "";
+  }
+
+  // 3. Loop over length and call appropriate random function for each type
+  for (i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  // 4. Add final password to password var and return
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+}
+
+// Generator functions - http://www.net-comber.com/charset.html
 
 function getRandomLowerCase() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-console.log(getRandomLowerCase());
+function getRandomUpperCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
